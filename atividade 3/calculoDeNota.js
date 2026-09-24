509e8ed9e944c46b72661433476aa345cef1c642
gabrielsimionicordova-lab <gabriel.simioni.cordova@escola.pr.gov.br>
2026-09-24 08:03:15 -0300

    Add files via upload

--- atividade 3/calculoDeNota.js
+++ atividade 3/calculoDeNota.js
@@ -6,7 +6,7 @@ function calcular() {
     nota1tri = Number(prompt("qual foi sua nota no primeiro trimestre?"));
     nota2tri = Number(prompt("qual foi sua nota no segundo trimestre?"));
 
-    resultado = 180 - nota1tri + nota2tri;
+    resultado = 180 - (nota1tri + nota2tri);
 
-    
+    alert(resultado);
 }
